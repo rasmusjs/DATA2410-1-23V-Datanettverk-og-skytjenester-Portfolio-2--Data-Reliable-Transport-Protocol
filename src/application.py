58dd9d88fae1e497f174da94f039a0d24941e880
Rasmus Jonssønn Skramstad <rasmus.skramstad@gmail.com>
2023-04-22 13:51:50 +0200

    Slo sammen argparse, og lagde headers for å kunne sende og motta informasjon.

--- src/application.py
+++ src/application.py
@@ -5,6 +5,7 @@ import time
 import re
 import sys
 import os
+import struct
 
 # Default values
 formatting_line = "-" * 45  # Formatting line = -----------------------------
@@ -23,177 +24,38 @@ def print_error(error_message):
     print(f"\033[1;31;1mError: \n\t{error_message}\n\033[0m", file=sys.stderr)
 
 
+# Define the structure of the headed
+# L = 32 bits, H = 16 bits
+# Sequence Number:32 bits, Acknowledgment Number:32, Flags:16 ,Window:16
+# From https://docs.python.org/3/library/struct.html
+protocol_struct = struct.Struct("!LLHH")
+
+
 # Description:
-# Checks is the input is a positive integer, raises an error if it's not.
+#  Function for creating a header with the right format with fixed bit sizes
 # Parameters:
-# integer: holds the integer to check
+#   sequence_number: holds the sequence number
+#   acknowledgment_number: holds the acknowledgment number
+#   flags: holds the flags
+#   window: holds the window
 # Returns:
-#   Returns the integer if valid, else it will exit the program with an error message
-def check_positive_integer(integer):
-    # Default error message message
-    error_message = None
-    try:
-        error_message = "expected an integer but you entered a string"
-        integer = int(integer)  # Try to cast to integer
-        if 0 >= integer:  # Check if it is a positive number
-            error_message = str(integer) + " is not a valid number, must be positive"
-            raise ValueError  # Raise error if it is not a positive number
-    except ValueError:  # Catch the error if it is not a positive number or not an integer
-        print_error(error_message)  # Print using standard error message function
-        parser.print_help()
-        exit(1)  # Exit the program
-    # Return the integer if it is a positive number
-    return integer
+#   Returns the header as a byte string
+def create_header(sequence_number, acknowledgment_number, flags, window):
+    # Sequence Number:32 bits, Acknowledgment Number:32bits, Flags:16bits, Window:16bits
+    return protocol_struct.pack(sequence_number, acknowledgment_number, flags, window)
 
 
 # Description:
-#   Checks if an integer from and including 1024 and up to and including 65,535
+#  Function for parsing a header
 # Parameters:
-#   port: holds the port number for server or client
+#   header: holds the header
 # Returns:
-#   Returns the port (integer) if valid, else it will exit the program with an error message
-def check_port(port):
-    # Default error message message
-    error_message = None
-    try:
-        error_message = "expected an integer but you entered a string"
-        port = int(port)
-        # Check if port is in range 1024-65535, else raise error_message
-        if port < 1024:
-            error_message = "Port number is too small, the port must be from 1024 upto 65535"
-            raise ValueError
-        if 65535 < port:
-            error_message = "Port number is too large, the port must be from 1024 upto 65535"
-            raise ValueError
-    except ValueError:
-        print_error(error_message)  # Print using standard error message function
-        parser.print_help()
-        exit(1)  # Exit the program
-
-    # Return the port number if it is valid
-    return port
+#   Returns the header as a tuple
+def parse_header(header):
+    return protocol_struct.unpack(header)
 
 
-# Description:
-#   Checks if an IP address is in dotted decimal notation.
-#   It checks the ranges of the numbers, and if it starts 0.
-# Parameters:
-#   ip: holds the ip address of the server
-# Returns
-#   the ip same ipaddress without leading zeroes, else it will exit the program with an error message
-def check_ipaddress(ip):
-    # Default error message message
-    error_message = None
-    # Split the ip into a list of octets
-    ip_split = ip.split(".")
-    # Clear the ip string
-    ip = ""
-    try:
-        # Check if we have 4 elements in the list i.e. 3 dots
-        if len(ip_split) != 4:
-            error_message = f"{ip} is not a valid ip. IPs must be in IPv4 format i.e in dotted decimal notation X.X.X.X"
-            raise ValueError
-
-        # Check if numbers are in range 0-255 and convert i.e. 01 to 1
-        for number in ip_split:
-            if not number.isdigit():
-                error_message = f"{number} is not a valid number, must be between 0-255"
-                raise ValueError
-
-            if 0 > int(number):
-                error_message = f"{number} is not a valid number, must be positive in the range 0-255"
-                raise ValueError
-
-            # If number is larger
-            if 255 < int(number):
-                error_message = f"{number} is not a valid number, must be smaller than 255"
-                raise ValueError
-
-            # Check if its starts with 0
-            if int(ip_split[0]) == 0:
-                error_message = f"{ip} ip cannot start with 0"
-                raise ValueError
-
-            # Convert i.e 01 to 1
-            ip += f"{int(number)}."
-    except ValueError:
-        print_error(error_message)  # Print using standard error_message message function
-        parser.print_help()
-        exit(1)  # Exit the program
-
-    # Remove the last dot
-    ip = ip[:-1]
-    return ip
-
-
-# Description:
-#   Checks if a path exists
-# Parameters:
-#   the path: holds the path
-# Returns:
-#   Returns the path if it exists, else it will exit the program with an error message
-def check_save_path(path):
-    # Default error message message
-    error_message = None
-    try:
-        if not os.path.isdir(path):  # Check if the path is a directory
-            error_message = f"{path} is not a valid save path"  # Set error_message message
-            raise ValueError  # Raise error_message
-    except ValueError:
-        print_error(error_message)  # Print using standard error_message message function
-        parser.print_help()
-        exit(1)  # Exit the program
-    return path  # Return the path if it exists
-
-
-# Description:
-#   Checks if a file exists
-# Parameters:
-#   file: holds the file name
-# Returns:
-#   Returns the file name if it exists, else it will exit the program with an error message
-def check_file(file):
-    # Default error message message
-    error_message = None
-    try:
-        if not os.path.isfile(file):  # Check if the file exists
-            error_message = f"{file} does not exist"  # Set error_message message
-            raise ValueError
-    except ValueError:
-        print_error(error_message)  # Print using standard error_message message function
-        parser.print_help()
-        exit(1)  # Exit the program
-    return file  # Return the file name if it exists
-
-
-# Add description and epilog to the parser, this is for prettier help text
-parser = argparse.ArgumentParser(description="Simpleperf, a simple iPerf clone for testing network performance.",
-                                 epilog="end of help")
-
-# Client only arguments
-client_group = parser.add_argument_group('client')  # Create a group for the client arguments, for the help text
-client_group.add_argument('-c', '--client', action="store_true", help="Start in client mode")
-client_group.add_argument('-f', '--file', type=check_file, help="File to send to the server")
-client_group.add_argument('-i', '--serverip', type=check_ipaddress, default=default_ip,
-                          help="IP address to connect/bind to, in dotted decimal notation. Default %(default)s")
-
-# Server only arguments
-server_group = parser.add_argument_group('server')  # Create a group for the server arguments, for the help text
-server_group.add_argument('-s', '--server', action="store_true", help="Start in server mode")
-server_group.add_argument('-a', '--bind', type=check_ipaddress, default=default_ip,
-                          help="Bind the server to a specific ip address, in dotted decimal notation. Default %("
-                               "default)s")
-
-server_group.add_argument('-sp', '--server_save_path', type=check_save_path, default=default_server_save_path,
-                          help="Path to save items. Default %("
-                               "default)s")
-
-# Common arguments
-parser.add_argument('-p', '--port', type=check_port, default=default_port,
-                    help="Port to use, default default %(default)s")
-
-# Parses the arguments from the user, it calls the check functions to validate the inputs given
-args = parser.parse_args()
+print(parse_header(create_header(1234, 5678, 1, 1)))
 
 
 # Description:
@@ -293,27 +155,187 @@ def start_client(ip, port, filename):
 # Startet med arparse. Det er fortsatt ikke helt opplagt hva flaggene er, men jeg antar at -m og -r er de samme greiene.
 # -m flagget: server og -r flagget: client
 def main():
-    parser = argparse.ArgumentParser(description="DRTP file transfer application script")
-    parser.add_argument("-c", action="store_true", help="Run in client mode")
-    parser.add_argument("-s", action="store_true", help="Run in server mode")
-    parser.add_argument("-f", type=str, help="Name of the file")
-    parser.add_argument("-m", type=str, choices=["stop_and_wait", "gbn", "sr"],
-                        help="Choose Reliability mode in the Server")
-    parser.add_argument("-r", type=str, choices=["stop_and_wait", "gbn", "sr"], default="stop_and_wait",
-                        help="Choose Reliability mode in the Client")
-    parser.add_argument("-t", type=str, choices=["loss", "skipack"], help="Choose your test mode")
-    parser.add_argument("-p", type=int, help="Port number")
+    # Description:
+    # Checks is the input is a positive integer, raises an error if it's not.
+    # Parameters:
+    # integer: holds the integer to check
+    # Returns:
+    #   Returns the integer if valid, else it will exit the program with an error message
+    def check_positive_integer(integer):
+        # Default error message message
+        error_message = None
+        try:
+            error_message = "expected an integer but you entered a string"
+            integer = int(integer)  # Try to cast to integer
+            if 0 >= integer:  # Check if it is a positive number
+                error_message = str(integer) + " is not a valid number, must be positive"
+                raise ValueError  # Raise error if it is not a positive number
+        except ValueError:  # Catch the error if it is not a positive number or not an integer
+            print_error(error_message)  # Print using standard error message function
+            parser.print_help()
+            exit(1)  # Exit the program
+        # Return the integer if it is a positive number
+        return integer
 
+    # Description:
+    #   Checks if an integer from and including 1024 and up to and including 65,535
+    # Parameters:
+    #   port: holds the port number for server or client
+    # Returns:
+    #   Returns the port (integer) if valid, else it will exit the program with an error message
+    def check_port(port):
+        # Default error message message
+        error_message = None
+        try:
+            error_message = "expected an integer but you entered a string"
+            port = int(port)
+            # Check if port is in range 1024-65535, else raise error_message
+            if port < 1024:
+                error_message = "Port number is too small, the port must be from 1024 upto 65535"
+                raise ValueError
+            if 65535 < port:
+                error_message = "Port number is too large, the port must be from 1024 upto 65535"
+                raise ValueError
+        except ValueError:
+            print_error(error_message)  # Print using standard error message function
+            parser.print_help()
+            exit(1)  # Exit the program
+
+        # Return the port number if it is valid
+        return port
+
+    # Description:
+    #   Checks if an IP address is in dotted decimal notation.
+    #   It checks the ranges of the numbers, and if it starts 0.
+    # Parameters:
+    #   ip: holds the ip address of the server
+    # Returns
+    #   the ip same ipaddress without leading zeroes, else it will exit the program with an error message
+    def check_ipaddress(ip):
+        # Default error message message
+        error_message = None
+        # Split the ip into a list of octets
+        ip_split = ip.split(".")
+        # Clear the ip string
+        ip = ""
+        try:
+            # Check if we have 4 elements in the list i.e. 3 dots
+            if len(ip_split) != 4:
+                error_message = f"{ip} is not a valid ip. IPs must be in IPv4 format i.e in dotted decimal notation X.X.X.X"
+                raise ValueError
+
+            # Check if numbers are in range 0-255 and convert i.e. 01 to 1
+            for number in ip_split:
+                if not number.isdigit():
+                    error_message = f"{number} is not a valid number, must be between 0-255"
+                    raise ValueError
+
+                if 0 > int(number):
+                    error_message = f"{number} is not a valid number, must be positive in the range 0-255"
+                    raise ValueError
+
+                # If number is larger
+                if 255 < int(number):
+                    error_message = f"{number} is not a valid number, must be smaller than 255"
+                    raise ValueError
+
+                # Check if its starts with 0
+                if int(ip_split[0]) == 0:
+                    error_message = f"{ip} ip cannot start with 0"
+                    raise ValueError
+
+                # Convert i.e 01 to 1
+                ip += f"{int(number)}."
+        except ValueError:
+            print_error(error_message)  # Print using standard error_message message function
+            parser.print_help()
+            exit(1)  # Exit the program
+
+        # Remove the last dot
+        ip = ip[:-1]
+        return ip
+
+    # Description:
+    #   Checks if a path exists
+    # Parameters:
+    #   the path: holds the path
+    # Returns:
+    #   Returns the path if it exists, else it will exit the program with an error message
+    def check_save_path(path):
+        # Default error message message
+        error_message = None
+        try:
+            if not os.path.isdir(path):  # Check if the path is a directory
+                error_message = f"{path} is not a valid save path"  # Set error_message message
+                raise ValueError  # Raise error_message
+        except ValueError:
+            print_error(error_message)  # Print using standard error_message message function
+            parser.print_help()
+            exit(1)  # Exit the program
+        return path  # Return the path if it exists
+
+    # Description:
+    #   Checks if a file exists
+    # Parameters:
+    #   file: holds the file name
+    # Returns:
+    #   Returns the file name if it exists, else it will exit the program with an error message
+    def check_file(file):
+        # Default error message message
+        error_message = None
+        try:
+            if not os.path.isfile(file):  # Check if the file exists
+                error_message = f"{file} does not exist"  # Set error_message message
+                raise ValueError
+        except ValueError:
+            print_error(error_message)  # Print using standard error_message message function
+            parser.print_help()
+            exit(1)  # Exit the program
+        return file  # Return the file name if it exists
+
+    # Add description and epilog to the parser, this is for prettier help text
+    parser = argparse.ArgumentParser(description="DRTP file transfer application script",
+                                     epilog="end of help")
+
+    # Client only arguments
+    client_group = parser.add_argument_group('Client')  # Create a group for the client arguments, for the help text
+    client_group.add_argument('-c', '--client', action="store_true", help="Run in client mode")
+    client_group.add_argument('-a', '--serverip', type=check_ipaddress, default=default_ip,
+                              help="Bind the server to a specific ip address, in dotted decimal notation. Default %("
+                                   "default)s")
+
+    # Server only arguments
+    server_group = parser.add_argument_group('Server')  # Create a group for the server arguments, for the help text
+    server_group.add_argument('-s', '--server', action="store_true", help="Run in server mode")
+    server_group.add_argument('-b', '--bind', type=check_ipaddress, default=default_ip,
+                              help="IP address to connect/bind to, in dotted decimal notation. Default %(default)s")
+    server_group.add_argument('-sp', '--server_save_path', type=check_save_path, default=default_server_save_path,
+                              help="Path to save items. Default %(""default)s")
+    # DRTP arguments
+    drtp_group = parser.add_argument_group('Protocol')  # Create a group for the DRTP arguments, for the help text
+    drtp_group.add_argument('-r', '--reliability', type=str, choices=["stop_and_wait", "gbn", "sr"],
+                            help="Choose Reliability mode")
+    drtp_group.add_argument('-t', '--mode', type=str, choices=["loss", "skipack"], help="Choose your test mode")
+
+    # Common arguments
+    parser.add_argument('-p', '--port', type=check_port, default=default_port,
+                        help="Port to use, default default %(default)s")
+    parser.add_argument('-f', '--file', type=check_file, help="Name of the file")
+
+    # Parses the arguments from the user, it calls the check functions to validate the inputs given
     args = parser.parse_args()
-    if args.c and args.s:
-        print("Cannot run as both client and server!")
+    if args.client and args.server:
+        print_error("Cannot run as both client and server!")
         sys.exit(1)
-    if args.c:
-        run_client(args.port, args.f, args.r, args.t)
-    elif args.s:
-        run_server(args.port, args.f, args.m, args.t)
+    if args.client:
+        print("Client mode")
+        # run_client(args.port, args.f, args.r, args.t)
+    elif args.server:
+        print("Server mode")
+        # run_server(args.port, args.f, args.m, args.t)
     else:
         print("Error, you must select server or client mode!")
+        parser.print_help()
         sys.exit(1)
 
     # Dette er den gamle main funksjonen, som ikke fungerer med argparse enda
